f81a64017a3a2be2a5cbb02bec94fb90cbbf2a6a
Emma Brillhart <emmabrillhart@Emmas-MacBook-Pro.local>
2021-03-01 14:28:52 -0700

    list of tier one criteria

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './criteria-data.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Tier One</h2>
+      <h3>Team Skills</h3>
+        <ul>
+        {data.tierOne.teamSkills.map(skill => <li>{skill}</li>)}
+        </ul>
+      <h3>Craft</h3>
+        <h4>Design</h4>
+        <ul>
+        {data.tierOne.craft.design.map(skill => <li>{skill}</li>)}
+        </ul>
+        <h4>Engineering</h4>
+        <ul>
+        {data.tierOne.craft.engineering.map(skill => <li>{skill}</li>)}
+        </ul>
     </div>
   );
 }
