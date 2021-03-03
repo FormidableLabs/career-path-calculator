19c85ce5cfc0e2a0eb686ff3ec75f3acc3f62cd8
Emma Brillhart <emmabrillhart@Emmas-MacBook-Pro.local>
2021-03-03 15:13:58 -0700

    prettier and linting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import React from 'react';
 import './App.css';
 import data from './criteria-data.json';
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Tier One</h2>
-      <h3>Team Skills</h3>
-        <ul>
-        {data.tierOne.teamSkills.map(skill => <li>{skill}</li>)}
-        </ul>
-      <h3>Craft</h3>
-        <h4>Design</h4>
-        <ul>
-        {data.tierOne.craft.design.map(skill => <li>{skill}</li>)}
-        </ul>
-        <h4>Engineering</h4>
-        <ul>
-        {data.tierOne.craft.engineering.map(skill => <li>{skill}</li>)}
-        </ul>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>Tier One</h2>
+            <h3>Team Skills</h3>
+            <ul>
+                {data.tierOne.teamSkills.map((skill) => (
+                    <li key={skill}>{skill}</li>
+                ))}
+            </ul>
+            <h3>Craft</h3>
+            <h4>Design</h4>
+            <ul>
+                {data.tierOne.craft.design.map((skill) => (
+                    <li key={skill}>{skill}</li>
+                ))}
+            </ul>
+            <h4>Engineering</h4>
+            <ul>
+                {data.tierOne.craft.engineering.map((skill) => (
+                    <li key={skill}>{skill}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
