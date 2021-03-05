20e9aa1f3a1414bfeac2262b33536d788868802a
Emma Brillhart <emmabrillhart@Emmas-MacBook-Pro.local>
2021-03-05 10:13:57 -0700

    add wouter for routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import './App.css';
-import data from './criteria-data.json';
+// import data from './criteria-data.json';
 
 function App() {
-    return (
-        <div className="App">
-            <h2>Tier One</h2>
-            <h3>Team Skills</h3>
-            <ul>
-                {data.tierOne.teamSkills.map((skill) => (
-                    <li key={skill}>{skill}</li>
-                ))}
-            </ul>
-            <h3>Craft</h3>
-            <h4>Design</h4>
-            <ul>
-                {data.tierOne.craft.design.map((skill) => (
-                    <li key={skill}>{skill}</li>
-                ))}
-            </ul>
-            <h4>Engineering</h4>
-            <ul>
-                {data.tierOne.craft.engineering.map((skill) => (
-                    <li key={skill}>{skill}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <button>Start Assessment</button>
+    </div>
+  );
 }
 
 export default App;
